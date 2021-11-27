7b6fb607c2a018fa8791f514564c0084aa5fb4ea
Verda-cpu <verdazhafif@email.com>
2021-11-27 14:58:54 +0700

    challenge-author

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $fillable = [
-        'title', 'description', 'author', 'publisher', 'date_of_issue', 'quantity'
+        'title', 'description', 'author', 'publisher', 'date_of_issue',
     ];
     public $timestamps = false;
+    
 }
